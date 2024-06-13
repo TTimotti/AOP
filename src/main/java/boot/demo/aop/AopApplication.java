ad966076186a9c25712aa89d44e4124ff5564024
TTimotti <ttimotti@kakao.com>
2024-06-13 14:16:55 +0900

    Update AopApplication.java
    시큐리티 설정 해제 (개발 중)

--- src/main/java/boot/demo/aop/AopApplication.java
+++ src/main/java/boot/demo/aop/AopApplication.java
@@ -2,8 +2,11 @@ package boot.demo.aop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableAspectJAutoProxy
 public class AopApplication {
 
     public static void main(String[] args) {
