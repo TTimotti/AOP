4ea2ce09d74398b539a7c79ba7fb3c33661ba089
TTimotti <ttimotti@kakao.com>
2024-06-13 14:16:44 +0900

    Create SecurityConfig.java

---
+++ src/main/java/boot/demo/aop/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package boot.demo.aop.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.authorizeRequests((authorizeRequest) ->
+               authorizeRequest.anyRequest().permitAll()
+            ).formLogin((formLogin) ->
+                    formLogin
+                            .usernameParameter("username")
+                            .passwordParameter("password")
+                            .defaultSuccessUrl("/", true)
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
