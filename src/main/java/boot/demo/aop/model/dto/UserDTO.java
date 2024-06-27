f7ff108aef3c6ae8d99fa9dadc7a91a701689752
TTimotti <ttimotti@kakao.com>
2024-06-27 11:23:16 +0900

    Update UserDTO.java

--- src/main/java/boot/demo/aop/model/dto/UserDTO.java
+++ src/main/java/boot/demo/aop/model/dto/UserDTO.java
@@ -3,12 +3,28 @@ package boot.demo.aop.model.dto;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
-public class UserDTO {
-    @NotEmpty(message = "아이디를 입력하세요")
+public class UserDTO implements UserDetails {
     private String username;
-    @NotEmpty(message = "비밀번호를 입력하세요")
     private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDetails [username=" + username + ", authorities=" + authorities + "]";
+    }
+
 }
