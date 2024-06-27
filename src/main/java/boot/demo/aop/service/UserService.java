e96a017d98cff50fdea4ac7d92002558aa7acba0
TTimotti <ttimotti@kakao.com>
2024-06-27 11:23:25 +0900

    Update UserService.java

--- src/main/java/boot/demo/aop/service/UserService.java
+++ src/main/java/boot/demo/aop/service/UserService.java
@@ -1,25 +1,60 @@
 package boot.demo.aop.service;
 
 import boot.demo.aop.aop.annotation.Loggable;
-import boot.demo.aop.model.TbUserMst;
-import boot.demo.aop.model.dto.UserDTO;
-import boot.demo.aop.repository.UserRepository;
+import boot.demo.aop.exception.AOPException;
+import boot.demo.aop.exception.ErrorCode;
+import boot.demo.aop.model.Code;
+import boot.demo.aop.model.entity.TbUserInfo;
+import boot.demo.aop.model.entity.TbUserMst;
+import boot.demo.aop.model.entity.TbUserRole;
+import boot.demo.aop.model.dto.SignUpRequestDTO;
+import boot.demo.aop.repository.TbUserInfoRepository;
+import boot.demo.aop.repository.TbUserMstRepository;
+import boot.demo.aop.repository.TbUserRoleRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
+    private final TbUserMstRepository tbUserMstRepository;
+    private final TbUserRoleRepository tbUserRoleRepository;
+    private final TbUserInfoRepository tbUserInfoRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Loggable
-    public UserDTO insertUserMst(UserDTO userDTO) {
-        TbUserMst newUserMst = TbUserMst.modelFromDto(userDTO);
-        int insertRows = userRepository.insertUserMst(newUserMst);
-        log.info("INSERT ROWS: {}", insertRows);
-        return TbUserMst.dtoFromModel(newUserMst);
+    @Transactional
+    public void insertUserMst(@Valid SignUpRequestDTO signUpRequestDTO) {
+        TbUserMst user = tbUserMstRepository.getUserMstById(signUpRequestDTO.getUsername());
+        if (user != null) {
+            throw new AOPException(ErrorCode.ALREADY_HAS_USER);
+        }
+
+        TbUserMst newUserMst = TbUserMst.builder()
+                .userId(signUpRequestDTO.getUsername())
+                .password(passwordEncoder.encode(signUpRequestDTO.getPassword()))
+                .build();
+
+        TbUserInfo newUserInfo = TbUserInfo.builder()
+                .userId(signUpRequestDTO.getUsername())
+                .name(signUpRequestDTO.getName())
+                .build();
+        // 기본은 ROLE_USER. 관리자의 경우 DB로 등록하거나 관리자 페이지에서 유저를 관리자로 등록 가능하도록 구현 예정.
+        TbUserRole newUserRole = TbUserRole.builder()
+                .userId(signUpRequestDTO.getUsername())
+                .roleCode(Code.ROLE_USER.getCode())
+                .build();
+
+
+        tbUserMstRepository.insertTbUserMst(newUserMst);
+        tbUserInfoRepository.insertTbUserInfo(newUserInfo);
+        tbUserRoleRepository.insertTbUserRole(newUserRole);
     }
+
 }
